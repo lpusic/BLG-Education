da822f6b8b062fa3cc5a8405fc7f0a5d16a4d797
lpusic <37160162+lpusic@users.noreply.github.com>
2020-02-10 13:26:37 -0500

    It really works!

--- IndividualKeys/run.sh
+++ IndividualKeys/run.sh
@@ -2,4 +2,5 @@
 
 
 echo 'Seed: ' $1
-./seed-converter-Darwin "$1"
+(./seed-converter-Darwin "$1") > privKey.txt
+python3 trim.py #cuts off extra words at the start of Darwin output
---
+++ IndividualKeys/trim.py
@@ -0,0 +1,15 @@
+privKey = ""
+
+with open ('privKey.txt', 'rt') as myfile:  # Open file lorem.txt for reading text
+    for myline in myfile:                 # For each line, read it to a string 
+    	print(myline)
+    	if myline == "Your ETH privkey derived from seed:":
+    		print(myline)					#Doesnt do anything, just holds that line
+    	else:
+    		privKey = myline
+
+
+print(privKey)								#creates new doc with private key
+f = open("privKey.txt", "w")
+f.write(privKey)
+f.close()

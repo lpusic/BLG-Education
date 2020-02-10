6c757d3f7ca1deeb3b31eafb5fcfc33397edf3d8
lpusic <37160162+lpusic@users.noreply.github.com>
2020-02-10 17:08:38 -0500

    some mods to the public key function and some simulation

--- Keys/PublicKeyFromPrivateKey/publicKeyFromPrivate.py
+++ Keys/PublicKeyFromPrivateKey/publicKeyFromPrivate.py
@@ -1,15 +1,17 @@
-import web3
-privKey = ''
-pubKey = ''
 
-file = open('../PrivateKeyFromRetina/privKey.txt','r')
-privKey = file.readlines()[0]
-privKey.replace('\n', '')
-print(privKey)
-file.close()
+def publicKeyFromPrivate():
+	import web3
+	privKey = ''
+	pubKey = ''
 
-pubKey = web3.Account.from_key(privKey)
+	file = open('../PrivateKeyFromRetina/privKey.txt','r') #Opens file that contains just private key
+	privKey = file.readlines()[0]						   #reads in the first line of the file, the private key
+	privKey.replace('\n', '')							   #cut off the /n at the end of the private key
+	file.close()										   #output a string that equals something like: 
+														   # 	"0x900f28ad68709df23c6191617152f0240ecd57760f707ff3bdbd489126db5064"
+	pubKey = web3.Account.from_key(privKey)   			   #Spits out 
+	print(pubKey)
 
-file = open('pubKey.txt','w')
-file.write(pubKey)
-file.close()
+# file = open('pubKey.','w')
+# file.write(pubKey)
+# file.close()
--- TransactionHash/sign.py
+++ TransactionHash/sign.py
@@ -2,11 +2,20 @@ import web3
 from web3.auto import w3
 from eth_account.messages import encode_defunct
 
-msg = "biologyPublicKey"
-private_key = "0x58ed59957b0dee44ba2e0a2709f20c2a4b6667442c8e149636537dc98e0e5970"
-message = encode_defunct(text=msg)
-signed_message = w3.eth.account.sign_message(message, private_key=private_key)
-print(signed_message)
+privKey = ''
 
+def sign(privateKey, msg):
+	msg = msg
+	private_key = privateKey
+	message = encode_defunct(text=msg)
+	signed_message = w3.eth.account.sign_message(message, private_key=private_key)
+	print(signed_message)
+
+def getPrivate():
+
+	file = open('../Keys/PrivateKeyFromRetina/privKey.txt','r') #Opens file that contains just private key
+	privKey = file.readlines()[0]						   #reads in the first line of the file, the private key
+	privKey.replace('\n', '')							   #cut off the /n at the end of the private key
+	file.close()
 
 #a1 = web3.Account.from_key(0x58ed59957b0dee44ba2e0a2709f20c2a4b6667442c8e149636537dc98e0e5970)
